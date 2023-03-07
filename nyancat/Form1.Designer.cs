2fb4b61fd9938dc6436691b71519ab8dbaa7b5d8
Antek Gzara <antek.gzara@gmail.com>
2023-03-07 08:59:54 +0100

    aadded nyan animation

--- nyancat/Form1.Designer.cs
+++ nyancat/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::nyancat.Properties.Resources._4;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(213, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(755, 447);
+            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1179, 577);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- nyancat/Form1.cs
+++ nyancat/Form1.cs
@@ -12,9 +12,61 @@ namespace nyancat
 {
     public partial class Form1 : Form
     {
+        int nyanAnimationFrame = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            Timer nyanTimer = new Timer();
+            nyanTimer.Interval = 50;
+            nyanTimer.Tick += new EventHandler(nyanAnimationTick);
+            nyanTimer.Start();
+        }
+
+        private void nyanAnimationTick(object sender, EventArgs e)
+        {
+            switch (nyanAnimationFrame) {
+                case 0:
+                    panel1.BackgroundImage = Properties.Resources._0;
+                    break;
+                case 1:
+                    panel1.BackgroundImage = Properties.Resources._1;
+                    break;
+                    case 2:
+                    panel1.BackgroundImage = Properties.Resources._2;
+                    break;
+                    case 3:
+                        panel1.BackgroundImage = Properties.Resources._3;
+                                        break;
+                    case 4:
+                    panel1.BackgroundImage = Properties.Resources._4;
+                    break;
+                    case 5:
+                    panel1.BackgroundImage = Properties.Resources._5;
+                                        break;
+                    case 6:
+                    panel1.BackgroundImage = Properties.Resources._6;
+                                        break;
+                    case 7:
+                    panel1.BackgroundImage = Properties.Resources._7;
+                                        break;
+                    case 8:
+                    panel1.BackgroundImage = Properties.Resources._8;
+                    break;
+                    case 9:
+                    panel1.BackgroundImage = Properties.Resources._9;
+                                        break;
+                    case 10:
+                    panel1.BackgroundImage = Properties.Resources._10;
+                                        break;
+                    case 11:
+                    panel1.BackgroundImage = Properties.Resources._11;
+                                        break;
+            }
+
+            nyanAnimationFrame++;
+            if(nyanAnimationFrame> 10) { nyanAnimationFrame = 0; }
         }
     }
 }
